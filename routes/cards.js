087b9915742eed8780659f28e2beb11c9e2a2852
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-30 11:31:31 +0300

    feat: connect handlers in routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,24 +1,7 @@
 const cardsRouter = require('express').Router();
-const path = require('path');
-const fs = require('fs');
 
-const cardsDataFilePath = path.join(__dirname, '../data/cards.json');
+const getCards = require('../controllers/cards');
 
-cardsRouter.get('/cards', (req, res) => {
-  fs.readFile(cardsDataFilePath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-        return;
-      }
-      res.status(500).send({ message: 'Внутренняя ошибка сервера' });
-      return;
-    }
-
-    const newData = JSON.parse(data);
-
-    res.send(newData);
-  });
-});
+cardsRouter.get('/cards', getCards);
 
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,9 @@
 const usersRouter = require('express').Router();
-const path = require('path');
-const fs = require('fs');
 
-const usersDataFilePath = path.join(__dirname, '../data/users.json');
+const { getUsers, getUserById } = require('../controllers/users');
 
-usersRouter.get('/users', (req, res) => {
-  fs.readFile(usersDataFilePath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-        return;
-      }
-      res.status(500).send({ message: 'Внутренняя ошибка сервера' });
-    }
+usersRouter.get('/users', getUsers);
 
-    const newData = JSON.parse(data);
-
-    res.send(newData);
-  });
-});
-
-usersRouter.get('/users/:id', (req, res) => {
-  fs.readFile(usersDataFilePath, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
-        return;
-      }
-      res.status(500).send({ message: 'Внутренняя ошибка сервера' });
-    }
-
-    const newData = JSON.parse(data);
-
-    const user = newData.find((item) => item._id === req.params.id);
-
-    if (!user) {
-      res.status(404).send({ message: 'Нет пользователя с таким id' });
-      return;
-    }
-
-    res.send(user);
-  });
-});
+usersRouter.get('/users/:id', getUserById);
 
 module.exports = usersRouter;
