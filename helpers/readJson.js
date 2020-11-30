542bd7f8798f516b447a19f954fbc146e62086aa
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-11-30 20:59:25 +0300

    hotfix: rethrowing error in getJson finc

--- helpers/readJson.js
+++ helpers/readJson.js
@@ -2,6 +2,8 @@ const fsPromises = require('fs').promises;
 
 const getJson = (filePath) => fsPromises.readFile(filePath, { encoding: 'utf-8' })
   .then((data) => JSON.parse(data))
-  .catch((err) => err);
+  .catch((err) => {
+    throw err;
+  });
 
 module.exports = getJson;
