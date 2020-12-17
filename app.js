83addf94b9ef7eac2a82719a65f27d4f25d59ec6
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-12-17 21:49:27 +0300

    feat: add body-parser

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const bodyParser = require('body-parser');
+
 const path = require('path');
 
 const cardsRoutes = require('./routes/cards');
 const usersRoutes = require('./routes/users');
 const notFountRouter = require('./routes/notFound');
 
+const app = express();
+
 const { PORT = 3000 } = process.env;
 
-const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
     useFindAndModify: false
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', cardsRoutes);
 
