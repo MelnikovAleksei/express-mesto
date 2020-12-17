afadf3b461dc73d2586b6bc08ec4b2f7fcf73705
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-12-17 16:23:38 +0300

    feat: install mongoose and connect the app to the server

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const path = require('path');
 
 const cardsRoutes = require('./routes/cards');
@@ -8,6 +10,12 @@ const notFountRouter = require('./routes/notFound');
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
