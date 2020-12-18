8f87e307142492b7ece90b29c4936db54247c225
MelnikovAleksei <melnikov.aleksei.alexeyevich@gmail.com>
2020-12-18 10:49:26 +0300

    feat: add temporary authorization solution

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 
 const bodyParser = require('body-parser');
 
-const path = require('path');
-
 const cardsRoutes = require('./routes/cards');
 const usersRoutes = require('./routes/users');
 const notFountRouter = require('./routes/notFound');
@@ -13,13 +11,22 @@ const app = express();
 
 const { PORT = 3000 } = process.env;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
     useFindAndModify: false
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5fdba7713713dd176fd708d2'
+  };
+
+  next();
 });
 
 app.use('/', cardsRoutes);
